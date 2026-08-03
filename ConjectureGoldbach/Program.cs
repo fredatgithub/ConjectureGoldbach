c770822edbce9b5759768dfeb5ec6f06e3f38dde
fred <domaine75@gmail.com>
2026-08-03 09:31:05 +0200

    Ajoutez des fichiers projet.

---
+++ ConjectureGoldbach/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConjectureGoldbach
+{
+  internal class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("Enter an even number greater than 2:");
+      int n = int.Parse(Console.ReadLine());
+      if (n <= 2 || n % 2 != 0)
+      {
+        Console.WriteLine("Invalid input. Please enter an even number greater than 2.");
+        return;
+      }
+
+      List<Tuple<int, int>> pairs = new List<Tuple<int, int>>();
+      for (int i = 2; i <= n / 2; i++)
+      {
+        if (IsPrime(i) && IsPrime(n - i))
+        {
+          pairs.Add(new Tuple<int, int>(i, n - i));
+        }
+      }
+
+      Console.WriteLine($"Goldbach's conjecture for {n}:");
+      foreach (var pair in pairs)
+      {
+        Console.WriteLine($"{pair.Item1} + {pair.Item2} = {n}");
+      }
+
+      Console.WriteLine("Press any key to exit...");
+      Console.ReadKey();
+    }
+
+    private static bool IsPrime(int i)
+    {
+      if (i <= 1) return false;
+      int sqrt = (int)Math.Sqrt(i);
+      for (int j = 2; j <= sqrt; j++)
+      {
+        if (i % j == 0) return false;
+      }
+
+      return true;
+    }
+  }
+}
